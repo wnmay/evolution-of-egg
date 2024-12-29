602faeb64e525e68c12372ce87660cbb4da7f058
wnmay <wanathachat@gmail.com>
2024-12-29 19:45:35 +0700

    feat: refactor button into  component

---
+++ src/components/Button.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+defineProps<{
+  buttonName: string;
+}>();
+</script>
+
+<template>
+  <button class="px-6 py-2 bg-thisYellow text-black font-semibold border-2 border-thisBlack rounded-lg hover:bg-yellow-500 focus:outline-none">
+    {{ buttonName }}
+    </button>
+</template>
